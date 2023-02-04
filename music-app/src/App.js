e54da839099425d26215ecaad28e401ef7a02c70
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-02-04 17:54:45 +0100

    delete button created

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import DeleteButton from './components/atoms/atom-DeleteButton/Atom-DeleteButton';
 
 function App() {
   return (
     <div className="App">
       <h1>Hola mundo</h1>
+      <DeleteButton/>
     </div>
   );
 }
