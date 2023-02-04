e749ca106b39419c14def93689c5b0766e8cf516
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-02-04 18:01:31 +0100

    update buttom created

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import UpdateButton from './components/atoms/atom-updateButton/atom-updateButton';
 
 function App() {
   return (
     <div className="App">
-     
+     <UpdateButton/>
     </div>
   );
 }
